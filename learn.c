68c6c40091026c625072f25de8e8b799fd68bf1b
tongzhouu <zhoudong@stu.sau.edu.cn>
2023-05-12 15:38:16 +0800

    socket connection process

---
+++ learn.c
@@ -0,0 +1,6 @@
+#include <sys/socket.h>
+
+/* int socket(int domain, int type, int protocol); */
+/* int bind(int sockfd, struct sockaddr *myaddr, socklen_t addrlen); */
+/* int listen(int sockfd, int backlog); */
+/* int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen); */
